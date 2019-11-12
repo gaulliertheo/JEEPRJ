09f4a95a4d1f25ac3af67d011f46ed4c8ff059ae
Unknown <gaullier.theo@gmail.com>
2019-11-12 18:37:01 +0100

    Suppression des anciennes versions et ajout Readme

--- README.md
+++
@@ -1,2 +0,0 @@
-# JEEPRJ
-Projet de JEE
--- jee_version1/src/java/dao/EmployeeDao.java
+++
@@ -1,8 +0,0 @@
-//package dao;
-//
-//import beans.Employee;
-//import java.util.List;
-//
-//public interface EmployeeDao {
-//    public List<Employee> getEmployees();
-//}
